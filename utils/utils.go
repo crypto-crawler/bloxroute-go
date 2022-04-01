8a350b6b354ea649f13c9fe6eec79509392fb0eb
billionwei <jesse.demain@gmail.com>
2022-04-01 14:03:30 +0000

    add SubscribeWalletBalance

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/big"
+	"strings"
+	"time"
 
 	"github.com/crypto-crawler/bloxroute-go/client"
 	"github.com/crypto-crawler/bloxroute-go/types"
@@ -19,6 +21,19 @@ type PairReserves struct {
 	BlockTimestampLast uint32         `json:"block_timestamp_last"`
 	BlockNumber        int64          `json:"block_number"`
 }
+
+type WalletBalance struct {
+	Address            common.Address `json:"address"`
+	Bnb                *big.Int       `json:"bnb"`
+	Usdt               *big.Int       `json:"usdt"`
+	Busd               *big.Int       `json:"busd"`
+	BlockTimestampLast int64          `json:"block_timestamp_last"`
+	BlockNumber        int64          `json:"block_number"`
+}
+
+var BNB = common.HexToAddress("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c")
+var USDT = common.HexToAddress("0x55d398326f99059fF775485246999027B3197955")
+var BUSD = common.HexToAddress("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56")
 
 func (p *PairReserves) Hash() uint64 {
 	h := md5.New()
@@ -63,6 +78,58 @@ func SubscribePairReserves(bloXrouteClient *client.BloXrouteClient, pairs []comm
 		}
 	}()
 	return bloXrouteClient.SubscribeEthOnBlock(nil, callParams, outChTmp)
+}
+
+// Monitor the balances of given addresses on Binance Chain. The balances are returned in BNB, USDT, BUSD.
+func SubscribeWalletBalance(bloXrouteClient *client.BloXrouteClient, addresses []common.Address, outCh chan<- *WalletBalance) error {
+	outChTmp := make(chan *types.EthOnBlockResponse)
+	callParams := make([]map[string]string, 0)
+	for _, address := range addresses {
+		name := fmt.Sprintf("bnb_%s", address.Hex())
+		callParams = append(callParams, map[string]string{"name": name, "method": "eth_getBalance", "address": address.Hex(), "data": "0x0"})
+		name = fmt.Sprintf("usdt_%s", address.Hex())
+		callParams = append(callParams, map[string]string{"name": name, "method": "eth_call", "to": USDT.Hex(), "data": buildBalanceInputData(address)})
+		name = fmt.Sprintf("busd_%s", address.Hex())
+		callParams = append(callParams, map[string]string{"name": name, "method": "eth_call", "to": BUSD.Hex(), "data": buildBalanceInputData(address)})
+	}
+
+	//holder := make(map[common.Address]*WalletBalance)
+	go func() {
+		for resp := range outChTmp {
+			var address common.Address
+			balance := &WalletBalance{}
+			if strings.HasPrefix(resp.Name, "bnb_") {
+				address = common.HexToAddress(resp.Name[len("bnb_"):])
+				balance.Bnb, _ = big.NewInt(0).SetString(resp.Response, 0)
+			} else if strings.HasPrefix(resp.Name, "usdt_") {
+				address = common.HexToAddress(resp.Name[len("usdt_"):])
+				balance.Usdt, _ = big.NewInt(0).SetString(resp.Response, 0)
+			} else if strings.HasPrefix(resp.Name, "busd_") {
+				address = common.HexToAddress(resp.Name[len("busd_"):])
+				balance.Busd, _ = big.NewInt(0).SetString(resp.Response, 0)
+			}
+			balance.Address = address
+			blockNumber, ok := big.NewInt(0).SetString(resp.BlockHeight, 0)
+			if !ok {
+				panic(resp)
+			}
+			balance.BlockNumber = blockNumber.Int64()
+			balance.BlockTimestampLast = time.Now().Unix()
+			//holder[address] = balance
+			outCh <- balance
+
+		}
+	}()
+	return bloXrouteClient.SubscribeEthOnBlock(nil, callParams, outChTmp)
+}
+
+func buildBalanceInputData(address common.Address) string {
+	funcSelector := []byte{0x70, 0xa0, 0x82, 0x31}
+	addressBytes := common.LeftPadBytes(address.Bytes(), 32)
+	var dataOut []byte
+	dataOut = append(dataOut, funcSelector...)
+	dataOut = append(dataOut, addressBytes...)
+	return fmt.Sprintf("0x%x", dataOut)
 }
 
 // Decode the data of ethOnBlock of GetReserves()
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"log"
 	"math/big"
 	"os"
 	"testing"
@@ -38,5 +39,44 @@ func TestSubscribePairReserves(t *testing.T) {
 	assert.Greater(t, pairReserves.Reserve0.Cmp(big.NewInt(0)), 0)
 	assert.Greater(t, pairReserves.Reserve1.Cmp(big.NewInt(0)), 0)
 
+	close(stopCh)
+}
+
+func TestSubscribeWalletBalance(t *testing.T) {
+	certFile := os.Getenv("BLOXROUTE_CERT_FILE")
+	keyFile := os.Getenv("BLOXROUTE_KEY_FILE")
+	if certFile == "" || keyFile == "" {
+		assert.FailNow(t, "Please provide the bloXroute cert and key files path in the environment variable variable")
+	}
+
+	stopCh := make(chan struct{})
+	bloXrouteClient, err := client.NewBloXrouteClientToCloud("BSC-Mainnet", certFile, keyFile, stopCh)
+	assert.NoError(t, err)
+
+	balancesCh := make(chan *WalletBalance)
+	addresses := []common.Address{
+		common.HexToAddress("0x95eA23508ecc3521081E72352C13707F6b179Fc1"),
+	}
+
+	err = SubscribeWalletBalance(bloXrouteClient, addresses, balancesCh)
+	assert.NoError(t, err)
+
+	balance := <-balancesCh
+	log.Println(balance)
+	if balance.Bnb != nil {
+		assert.Equal(t, balance.Bnb.String(), "2618353928878013888")
+	}
+
+	balance = <-balancesCh
+	log.Println(balance)
+	if balance.Bnb != nil {
+		assert.Equal(t, balance.Bnb.String(), "2618353928878013888")
+	}
+
+	balance = <-balancesCh
+	log.Println(balance)
+	if balance.Bnb != nil {
+		assert.Equal(t, balance.Bnb.String(), "2618353928878013888")
+	}
 	close(stopCh)
 }
