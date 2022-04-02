e2a0b86823260d9a397f7d8b03a4524161d2add8
soulmachine <soulmachine@gmail.com>
2022-04-02 00:33:23 +0000

    Implemented TransactionStatusClient

---
+++ client/transaction_status_client.go
@@ -0,0 +1,220 @@
+package client
+
+import (
+	"crypto/tls"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/crypto-crawler/bloxroute-go/types"
+	"github.com/gorilla/websocket"
+)
+
+type TransactionStatusClient struct {
+	conn           *websocket.Conn
+	subscriptionID string
+	stopCh         <-chan struct{}
+	outCh          chan<- *types.TxStatus
+	// To ensure there is only one concurrent conn.WriteMessage(), see
+	// https://pkg.go.dev/github.com/gorilla/websocket#hdr-Concurrency
+	mu                *sync.Mutex
+	commandResponseCh chan commandResponse
+}
+
+func NewTransactionStatusClient(authorizationHeader string, stopCh <-chan struct{}, outCh chan<- *types.TxStatus) (*TransactionStatusClient, error) {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	dialer := websocket.DefaultDialer
+	dialer.TLSClientConfig = tlsConfig
+	conn, _, err := dialer.Dial("wss://api.blxrbdn.com/ws", http.Header{"Authorization": []string{authorizationHeader}})
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte(`{"jsonrpc":"2.0","id":1,"method":"subscribe","params":["transactionStatus",{"include":["tx_hash","status"]}]}`))
+	if err != nil {
+		return nil, err
+	}
+
+	_, nextNotification, err := conn.ReadMessage()
+	if err != nil {
+		return nil, err
+	}
+	subResp := types.SubscriptionResponse{}
+	err = json.Unmarshal(nextNotification, &subResp)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	subscriptionID := subResp.Result
+	if subscriptionID == "" {
+		return nil, errors.New("subscriptionID is empty")
+	}
+
+	client := &TransactionStatusClient{
+		conn:              conn,
+		subscriptionID:    subscriptionID,
+		stopCh:            stopCh,
+		outCh:             outCh,
+		mu:                &sync.Mutex{},
+		commandResponseCh: make(chan commandResponse),
+	}
+
+	client.ping()
+	go client.run()
+
+	return client, nil
+}
+
+type commandResponse struct {
+	Id      int64  `json:"id"`
+	JsonRPC string `json:"jsonrpc"`
+	Result  *struct {
+		Success bool `json:"success"`
+	} `json:"result,omitempty"` // subscription ID is here
+}
+
+func (client *TransactionStatusClient) StartMonitorTransaction(transactions [][]byte, monitorSpeedup bool) error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+
+	arr := make([]string, len(transactions))
+	for i, tx := range transactions {
+		arr[i] = hex.EncodeToString(tx)
+	}
+	bytes, _ := json.Marshal(arr)
+	arrJson := string(bytes)
+	subRequest := fmt.Sprintf(`{"jsonrpc":"2.0","id":1,"method":"start_monitor_transaction","params":{"transactions":%s,"monitor_speedup":"%v"}}`, arrJson, monitorSpeedup)
+
+	err := client.conn.WriteMessage(websocket.TextMessage, []byte(subRequest))
+	if err != nil {
+		return err
+	}
+
+	success := client.waitForCommandResponse()
+	if !success {
+		return errors.New("failed to start monitor transaction")
+	}
+
+	return nil
+}
+
+func (client *TransactionStatusClient) StopMonitorTransaction(transactions [][]byte, monitorSpeedup bool) error {
+	arr := make([]string, len(transactions))
+	for i, tx := range transactions {
+		arr[i] = hex.EncodeToString(tx)
+	}
+	bytes, _ := json.Marshal(arr)
+	arrJson := string(bytes)
+	subRequest := fmt.Sprintf(`{"jsonrpc":"2.0","id":1,"method":"stop_monitor_transaction","params":{"transactions":%s}}`, arrJson)
+
+	err := client.conn.WriteMessage(websocket.TextMessage, []byte(subRequest))
+	if err != nil {
+		return err
+	}
+
+	_, nextNotification, err := client.conn.ReadMessage()
+	if err != nil {
+		return err
+	}
+	commandResp := commandResponse{}
+	err = json.Unmarshal(nextNotification, &commandResp)
+	if err != nil {
+		return err
+	}
+	if !commandResp.Result.Success {
+		return errors.New(string(nextNotification))
+	}
+
+	return nil
+}
+
+func (client *TransactionStatusClient) waitForCommandResponse() bool {
+	select {
+	case <-time.After(3 * time.Second):
+		return false
+	case resp := <-client.commandResponseCh:
+		return resp.Result.Success
+	}
+}
+
+// Sends a ping message every 5 seconds to keep the WebSocket connection alive.
+// https://docs.bloxroute.com/apis/ping
+func (client *TransactionStatusClient) ping() {
+	ticker := time.NewTicker(5 * time.Second) // ping every 5 seconds
+	go func() {
+		for {
+			select {
+			case <-client.stopCh:
+				return
+			case <-ticker.C:
+				client.mu.Lock()
+				defer client.mu.Unlock()
+				err := client.conn.WriteMessage(websocket.TextMessage, []byte(`{"method": "ping"}`))
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}()
+}
+
+// Close is used to terminate our websocket client
+func (client *TransactionStatusClient) close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	err := client.conn.WriteMessage(
+		websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""),
+	)
+	client.conn.Close()
+	return err
+}
+
+// Run the event looop.
+func (client *TransactionStatusClient) run() error {
+	for {
+		select {
+		case <-client.stopCh:
+			client.close()
+			return nil
+		default:
+			_, nextNotification, err := client.conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			{
+				// Is it a command response?
+				commandResp := commandResponse{}
+				err = json.Unmarshal(nextNotification, &commandResp)
+				if err == nil {
+					if commandResp.Result != nil {
+						client.commandResponseCh <- commandResp
+						break
+					}
+				}
+			}
+
+			msg := types.WebsocketMsg[*types.TxStatus]{}
+			err = json.Unmarshal(nextNotification, &msg)
+			if err != nil {
+				return err
+			}
+			if len(msg.Error) > 0 {
+				return errors.New(string(nextNotification))
+			}
+
+			if msg.Params.Result.TxHash != "" {
+				client.outCh <- msg.Params.Result
+			}
+		}
+	}
+}
---
+++ client/transaction_status_client_test.go
@@ -0,0 +1,98 @@
+package client
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+	"time"
+
+	bloxroute_types "github.com/crypto-crawler/bloxroute-go/types"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/assert"
+)
+
+func TransactionByHash(ethClient *ethclient.Client, txHash common.Hash) (*types.Transaction, error) {
+	ctx := context.Background()
+	headerCh := make(chan *types.Header)
+
+	sub, err := ethClient.SubscribeNewHead(ctx, headerCh)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		select {
+		case <-time.After(time.Second * 10):
+			sub.Unsubscribe()
+			return nil, errors.New("timeout")
+		case <-headerCh:
+			tx, _, err := ethClient.TransactionByHash(ctx, txHash)
+			if tx != nil || err == nil {
+				return tx, nil
+			}
+		}
+	}
+}
+
+func TestStartMonitorTransaction(t *testing.T) {
+	var transactionStatusClient *TransactionStatusClient = nil
+	var bloXrouteClient *BloXrouteClient = nil
+	var err error = nil
+	stopCh := make(chan struct{})
+	txStatusCh := make(chan *bloxroute_types.TxStatus)
+	{
+		authorizationHeader := os.Getenv("AUTHORIZATION_HEADER")
+		if authorizationHeader == "" {
+			assert.FailNow(t, "Please provide the authorization header in the AUTHORIZATION_HEADER environment variables")
+		}
+		transactionStatusClient, err = NewTransactionStatusClient(authorizationHeader, stopCh, txStatusCh)
+		assert.NoError(t, err)
+		assert.NotNil(t, transactionStatusClient)
+	}
+	{
+		certFile := os.Getenv("BLOXROUTE_CERT_FILE")
+		keyFile := os.Getenv("BLOXROUTE_KEY_FILE")
+		if certFile == "" || keyFile == "" {
+			assert.FailNow(t, "Please provide the bloXroute cert and key files path in the environment variable variable")
+		}
+		bloXrouteClient, err = NewBloXrouteClientToCloud("BSC-Mainnet", certFile, keyFile, stopCh)
+		assert.NoError(t, err)
+		assert.NotNil(t, bloXrouteClient)
+	}
+
+	var ethClient *ethclient.Client = nil
+	{
+		fullnodeUrl := os.Getenv("FULLNODE_URL")
+		if fullnodeUrl == "" {
+			assert.FailNow(t, "Please provide the fullnode URL by setting the FULLNODE_URL environment variable")
+		}
+		ctx := context.Background()
+		ethClient, err = ethclient.DialContext(ctx, fullnodeUrl)
+	}
+	assert.NoError(t, err)
+	assert.NotNil(t, ethClient)
+
+	txHashCh := make(chan *bloxroute_types.Transaction)
+	err = bloXrouteClient.SubscribeNewTxs([]string{"tx_hash"}, "", txHashCh)
+	assert.NoError(t, err)
+
+	txHash := <-txHashCh
+	assert.NotEmpty(t, txHash.TxHash)
+
+	tx, err := TransactionByHash(ethClient, common.HexToHash(txHash.TxHash))
+	assert.NoError(t, err)
+	assert.NotNil(t, tx)
+
+	txRaw, err := tx.MarshalBinary()
+	assert.NoError(t, err)
+	err = transactionStatusClient.StartMonitorTransaction([][]byte{txRaw}, false)
+
+	txStatus := <-txStatusCh
+	assert.NotNil(t, txStatus)
+	assert.Equal(t, common.HexToHash(txStatus.TxHash), common.HexToHash(txHash.TxHash))
+
+	close(stopCh)
+}
